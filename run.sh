b475a259df1559fc0d8df9e05e64962cdb32f538
Jhordy Gavinchu <65508346+jhordyess@users.noreply.github.com>
2022-08-09 12:36:58 -0400

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#TODO Make an executable?
+printf "File name: "
+read File_path
+if [[ -f "$File_path" ]]; then
+    File_full_name=$(basename "$File_path")
+    File_extension=${File_full_name##*.}
+    if [[ "$File_extension" == "aco" ]]; then
+        File_name=${File_full_name%.*}
+        File_dir=$(dirname "$(realpath "$File_path")")
+        make -s && ./aco2gpl <"${File_path}" >"${File_dir}/${File_name}.gpl"
+        echo "File saved in: \"${File_dir}/${File_name}.gpl\""
+    else
+        echo "The file isn't an Adobe Photoshop color file(.aco)"
+    fi
+else
+    echo "The file \"$File_path\" does not exists."
+fi
